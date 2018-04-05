9fb2e3deb4a4965db2f26c71a07eee4d438afd37
Stephen Badger <stephen.badger@gmail.com>
2018-04-05 22:55:15 +0100

    Quote resources

--- vars/createBuildProject.groovy
+++ vars/createBuildProject.groovy
@@ -1,7 +1,7 @@
 def call() {
     openshift.withCluster() {
         openshift.newProject(getProjectName())
-        def sshKey = libraryResource com/pantou/build/pipeline/ssh-secret.yaml
+        def sshKey = libraryResource 'com/pantou/build/pipeline/ssh-secret.yaml'
         openshift.withProject(getProjectName()) {
             openshift.apply(sshKey)
         }
