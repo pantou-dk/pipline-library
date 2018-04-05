39f5af424553cfc8459c5d8a40180ac32e2f6efc
Stephen Badger <stephen.badger@gmail.com>
2018-04-05 23:44:29 +0100

    Don't create SSH key credentials
    We should not need them for builds, as Jenkins will have checked everything out.

--- vars/createBuildProject.groovy
+++ vars/createBuildProject.groovy
@@ -1,9 +1,5 @@
 def call() {
     openshift.withCluster() {
         openshift.newProject(getProjectName())
-        def sshKey = libraryResource 'com/pantou/build/pipeline/ssh-secret.yaml'
-        openshift.withProject(getProjectName()) {
-            openshift.apply(sshKey)
-        }
     }
 }
