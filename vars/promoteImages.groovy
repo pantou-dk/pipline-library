38e58a40f68a125ffb066a8ae0abec10d2c52b80
Stephen Badger <stephen.badger@gmail.com>
2018-04-06 19:42:49 +0100

    Adds image promotion based upon label

--- vars/promoteImages.groovy
+++ vars/promoteImages.groovy
@@ -1,2 +1,13 @@
 def call(String fromProject, String toProject) {
+    openshift.withCluster() {
+        def images = '';
+        openshift.withProject(fromProject) {
+            images = openshift.selector('is', ['promote': 'true']).freeze()
+        }
+        openshift.withProject(toProject) {
+            images.withEach {
+                openshift.tag(fromProject + '/' + it.name + ':latest', it.name + ':latest')
+            }
+        }
+    }
 }
